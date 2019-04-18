14069b6d2f4517bda41d30aec6e65dc70566fe54
AnshPrakash <anshprakash1997@gmail.com>
2019-04-19 00:05:51 +0530

    CNN Model not working properly Code to generate test Set is Done

--- CNNModel.py
+++ CNNModel.py
@@ -7,9 +7,13 @@ from torch.utils.data import Dataset,DataLoader
 import matplotlib.pyplot as plt
 from skimage import io, transform
 from torch.autograd import Variable
+import numpy as np
 import cv2
 import os
 
+
+# Check if gpu support is available
+cuda_avail = torch.cuda.is_available()
 
 class BreakOutDataset(Dataset):
     """BreakOut dataset."""
@@ -68,20 +72,33 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         self.conv1 = nn.Conv2d(in_channels = 3,out_channels = 32, kernel_size = 3,stride =2,padding =(0,0))
+        self.bn1 = nn.BatchNorm2d(32)
         self.pool1 = nn.MaxPool2d(kernel_size=2,stride = 2)
         self.conv2 = nn.Conv2d(in_channels = 32,out_channels = 64, kernel_size = 3,stride =2,padding =(0,0))
+        self.bn2 = nn.BatchNorm2d(64)
         self.pool2 = nn.MaxPool2d(kernel_size=2,stride = 2)
         self.fc = nn.Linear(37440,2048)
-        self.out_layer = nn.Linear(2048, 10)
+        self.out_layer = nn.Linear(2048, 2)
 
     def forward(self, x):
         x = F.relu(self.conv1(x))
+        # print("After Conv1",x)
+        x = self.bn1(x)
         x = self.pool1(x)
+        # print("After Max pool1",x)
         x = F.relu(self.conv2(x))
+        x = self.bn2(x)
+        # print("After conv 2",x)
         x = self.pool2(x)
+        # print("After Max pool 2",x)
         x = x.view(-1, self.num_flat_features(x))
+        # print(x)
         x = F.relu(self.fc(x))
+        # print("After Hideen Layer",x)
         x = self.out_layer(x)
+        # print("After Output layer",x)
+        x = F.softmax(x,dim=1)
+        # print(x)
         return x
 
     def num_flat_features(self, x):
@@ -91,28 +108,55 @@ class Net(nn.Module):
             num_features *= s
         return num_features
 
+def accuracy(file):
+    accuracy = 0
+    total =0
+    TrainingDataset = BreakOutDataset(file=file)
+    dataloader = DataLoader(TrainingDataset, batch_size=4,shuffle=False, num_workers=4)
+    for i_batch, sample_batched in enumerate(dataloader):
+        if cuda_avail:
+            train_data = (sample_batched['image'].cuda())
+            target = (sample_batched['label'].cuda())
+        else:                
+            train_data = (sample_batched['image'])
+            target = (sample_batched['label'])
+        with torch.no_grad():
+            pred = net(train_data)
+            total +=(len(target))
+            # print(len(target))
+            maxs, argmax = torch.max(pred, 1)
+            accuracy += torch.sum(torch.eq(argmax,target)).float()
+    return(accuracy/total)
 
-net = Net()
+
+
+net = Net().cuda() if cuda_avail else Net()
+print("Cuda ",cuda_avail)
+
+
 criterion = nn.NLLLoss()
-optimizer = optim.SGD(net.parameters(), lr=0.01)
+optimizer = optim.SGD(net.parameters(), lr=0.00001,momentum = 0.9,weight_decay=1e-5)
 epochs =1
+batch_size = 4
 
 for _ in range(epochs):
-    # for i in range(1,501):
-    for i in range(1,2):
+    for i in range(1,501):
         TrainingDataset = BreakOutDataset(file='TrainingData/'+str(i).zfill(8))
-        dataloader = DataLoader(TrainingDataset, batch_size=4,shuffle=False, num_workers=4)
+        dataloader = DataLoader(TrainingDataset, batch_size=batch_size,shuffle=False, num_workers=4)
         for i_batch, sample_batched in enumerate(dataloader):
-            inputs = Variable(sample_batched['image'])
-            target = Variable(sample_batched['label'])
+            if cuda_avail:
+                inputs = Variable(sample_batched['image'].cuda())
+                target = Variable(sample_batched['label'].cuda())
+            else:                
+                inputs = Variable(sample_batched['image'])
+                target = Variable(sample_batched['label'])
             optimizer.zero_grad()   # zero the gradient buffers
             output = net(inputs)
-            print(output,target)
-            loss = criterion(output, target)
+            loss = criterion(output.cpu(), target.cpu())
             loss.backward()
             optimizer.step()    # Does the update
+            # print(torch.max(output,1)[-1],target)
+            # print("probabilties ",output)
+            # print(str(loss) +"\n")
+        print("Accuracy ",accuracy('TrainingData/'+str(i).zfill(8)))
 
-# with torch.no_grad():
-#         for param in model.parameters():
-#             param -= learning_rate * param.grad
-#             
---
+++ createSVMData.py
@@ -0,0 +1,85 @@
+import os
+import numpy as np
+import pandas as pd
+import random
+
+
+def selectSample(l,n):
+	random.shuffle(l)
+	b = l[:n]
+	b.sort()
+	return(b)
+
+
+
+def getRewardedSample(indices,data):
+	samples = []
+	idxStart = indices - 7
+	for idx in idxStart:
+		l = [i +idx for i in range(6)]
+		l = selectSample(l,4)
+		l.append(idx + 6)
+		l = np.array(l) 
+		fseq = np.array(data.iloc[l+1]).reshape(1,-1)
+		samples.append((fseq,1.0))
+	return(samples)
+
+
+
+def getRandomSamples(labels,data):
+	samples = []
+	start = int(np.random.rand()*10) + 1
+	stride = 0
+	while start < (len(labels)-8):
+		l = [0,1,2,3,4,5]
+		l = selectSample(l,4)+[6]
+		temp = np.array(data.iloc[np.array(l)+1]).reshape(-1)
+		samples.append((temp,labels.iloc[start + 6,-1]))
+		stride = random.randint(20,30)
+		start += stride
+	return(samples)
+
+
+
+
+try:
+	os.mkdir("SVMInput")
+except Exception as e:
+	print(e)
+
+
+os.chdir('PCA_Out')
+
+samples = [] 
+for i in range(1,501):
+	print("Processing Game",i)
+	label_file = str(i)+"_rew.csv"
+	data_file = "transformed_data_" + str(i).zfill(8) + ".csv"
+	with open(label_file) as f:
+		labels = pd.read_csv(f,header = None)
+	with open(data_file) as f:
+		data = pd.read_csv(f)
+		data = data.iloc[:,1:]
+	indxOfRewards = labels.index[labels[0]== 1] # We have rename the Indices
+	samples = samples + getRewardedSample(indxOfRewards,data)
+	samples = samples + (getRandomSamples(labels,data))
+
+os.chdir("../SVMInput")
+random.shuffle(samples)
+
+final_data = [0]*len(samples)
+final_labels  = [0]*len(samples)
+for i in range(len(samples)):
+	final_data[i]   = (samples[i][0])
+	final_labels[i] = (samples[i][1])
+
+del samples
+final_data = np.array(final_data)
+final_labels = np.array(final_labels)
+
+data_df = pd.DataFrame(final_data)
+labels_df = pd.DataFrame(final_labels)
+data_df.to_csv("FINAL_data.csv")
+labels_df.to_csv("FINAL_labels.csv")
+
+print("Processing Done SVM Data Done")
